55bfccd384edbeb35969b3e1ceeef9b2e36eceb3
Stefan <stefan.kovacs.940@my.csun.edu>
2021-11-02 12:44:39 -0700

    added snort installs (prereqs and other features

--- installation.sh
+++ installation.sh
@@ -67,11 +67,121 @@ sudo service apache2 restart
 echo "Installing iptables"
 sudo apt-get install iptables
 
-echo "Installing Snort"
-
-
 echo "Installing OpenSSH"
 sudo apt-get install openssh
+
+echo "Installing wget"
+apt-get install wget
+
+echo "Installing make"
+sudo apt-get install -y make
+
+echo "Installing cmake"
+sudo apt-get -y install cmake
+
+echo "Time to install snort!"
+
+echo "Creating Snort src dir"
+sudo dpkg-reconfigure tzdata
+mkdir ~/snort_src
+cd ~/snort_src
+
+echo "Installing Snort Prereqs"
+sudo apt-get install -y build-essential autotools-dev libdumbnet-dev libluajit-5.1-dev libpcap-dev \
+	zlib1g-dev pkg-config libhwloc-dev cmake liblzma-dev openssl libssl-dev cpputest libsqlite3-dev \
+	libtool uuid-dev git autoconf bison flex libcmocka-dev libnetfilter-queue-dev libunwind-dev \
+	libmnl-dev ethtool
+
+echo "Installing safec for runtime bounds checks"
+cd ~/snort_src
+wget https://github.com/rurban/safeclib/releases/download/v02092020/libsafec-02092020.tar.gz
+tar -xzvf libsafec-02092020.tar.gz
+cd libsafec-02092020.0-g6d921f
+./configure
+make
+sudo make install
+
+echo "Installing Perl Compatible Regular Expressions"
+cd ~/snort_src/
+wget https://ftp.pcre.org/pub/pcre/pcre-8.45.tar.gz
+tar -xzvf pcre-8.45.tar.gz
+cd pcre-8.45
+./configure
+make
+sudo make install
+
+echo "Installing gperftools 2.9"
+cd ~/snort_src
+wget https://github.com/gperftools/gperftools/releases/download/gperftools-2.9.1/gperftools-2.9.1.tar.gz
+tar xzvf gperftools-2.9.1.tar.gz
+cd gperftools-2.9.1
+./configure
+make
+sudo make install
+
+echo "Installing Ragel"
+cd ~/snort_src
+wget http://www.colm.net/files/ragel/ragel-6.10.tar.gz
+tar -xzvf ragel-6.10.tar.gz
+cd ragel-6.10
+./configure
+make
+sudo make install
+
+echo "Downloading Boost C++ libraries"
+cd ~/snort_src
+wget https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.gz
+tar -xvzf boost_1_76_0.tar.gz
+
+echo "Installing Hyperscan 5.4"
+cd ~/snort_src
+wget https://github.com/intel/hyperscan/archive/refs/tags/v5.4.0.tar.gz
+tar -xvzf v5.4.0.tar.gz
+mkdir ~/snort_src/hyperscan-5.4.0-build
+cd hyperscan-5.4.0-build/
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DBOOST_ROOT=~/snort_src/boost_1_76_0/ ../hyperscan-5.4.0
+make
+sudo make install
+
+echo "Installing flatbuffers"
+cd ~/snort_src
+wget https://github.com/google/flatbuffers/archive/refs/tags/v2.0.0.tar.gz -O flatbuffers-v2.0.0.tar.gz
+tar -xzvf flatbuffers-v2.0.0.tar.gz
+mkdir flatbuffers-build
+cd flatbuffers-build
+cmake ../flatbuffers-2.0.0
+make
+sudo make install
+
+echo "Installing Data Acquisition library"
+cd ~/snort_src
+wget https://github.com/snort3/libdaq/archive/refs/tags/v3.0.4.tar.gz -O libdaq-3.0.4.tar.gz
+tar -xzvf libdaq-3.0.4.tar.gz
+cd libdaq-3.0.4
+./bootstrap
+./configure
+make
+sudo make install
+
+echo "Updating shares libraries"
+sudo ldconfig
+
+echo "Installing Snort 3"
+cd ~/snort_src
+wget https://github.com/snort3/snort3/archive/refs/tags/3.1.6.0.tar.gz -O snort3-3.1.6.0.tar.gz
+tar -xzvf snort3-3.1.6.0.tar.gz
+cd snort3-3.1.6.0
+./configure_cmake.sh --prefix=/usr/local --enable-tcmalloc
+cd build
+make
+sudo make install
+
+echo "Confirm snort version"
+/usr/local/bin/snort -V
+
+echo "testing snort with the default config file"
+snort -c /usr/local/etc/snort/snort.lua
+
 
 echo 
 
