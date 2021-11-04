978c3477fc4d8848da4a33c9d4690eeba51b55ce
Stefan <stefan.kovacs.940@my.csun.edu>
2021-11-03 17:08:25 -0700

    Added snort configuration

--- implementation.sh
+++ implementation.sh
@@ -32,5 +32,49 @@ sudo netfilter-persistent save
 
 echo "iptables implemention completed"
 
+echo "Configuring snort"
+# Create directories
+sudo mkdir /usr/local/etc/rules
+sudo mkdir /usr/local/etc/so_rules/
+sudo mkdir /usr/local/etc/lists/
+sudo touch /usr/local/etc/rules/local.rules
+sudo touch /usr/local/etc/lists/default.blocklist
+sudo mkdir /var/log/snort
+
+# Create local.rules file
+sudo touch /usr/local/etc/rules/local.rules
+
+# This rule will detect ICMP trc, and is really good for testing that Snort is working correctly and generating alerts
+sudo echo alert icmp any any -> any any ( msg:"ICMP Traffic Detected"; sid:10000001; metadata:policy security-ips alert; ) > /usr/local/etc/rules/local.rules
+
+# Run Snort and have it load the local.rules file (with the -R flag) to make sure it loads these rules correctly
+# The output should end with “Snort successfully validated the configuration” 
+snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/rules/local.rules
+
+# run Snort in detection mode on an interface (change eth0 below to match your interface name), and print all alerts to the console
+#sudo snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/rules/local.rules \
+#	-i eth0 -A alert_fast -s 65535 -k none
+# (run command above to receive an output of ICMP alerts)
+
+# edit the snort.lua at line 169
+# sudo vi /usr/local/etc/snort/snort.lua
+
+# 169 ips =
+# 170 {
+# 171 enable_builtin_rules = true,
+# 172 include = RULE_PATH .. "/local.rules",
+# 173 variables = default_variables
+# 174 }
+
+# test config file
+# snort -c /usr/local/etc/snort/snort.lua
+
+# Now we can run snort as above, however we don’t explictly pass the local.rules file on the command line, as we’ve included it in the ips section in the snort.lua file
+# sudo snort -c /usr/local/etc/snort/snort.lua -i eth0 -A alert_fast -s 65535 -k none
+
+echo "Please follow the rest of the instructions for the snort configuration"
+
+echo "Snort and iptables implementation completed"
+
 echo ""
 exit 0
