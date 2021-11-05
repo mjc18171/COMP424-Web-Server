3e77f97cc12ff27e6ccb08b807b1a47d754bbf53
Stefan <stefan.kovacs.940@my.csun.edu>
2021-11-04 18:36:57 -0700

    Added composer install

--- installation.sh
+++ installation.sh
@@ -57,6 +57,21 @@ echo ""
 sleep 3
 
 echo ""
+echo "Installing Composer"
+echo "========================================================================================"
+sudo apt install php-cli unzip
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+echo $HASH
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+# Install Composer globally
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+echo""
+
+sleep 3
+
+echo ""
 echo "Installing PHP"
 echo "========================================================================================"
 # this is install will prompt you, select apache2
